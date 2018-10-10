aa37c64a44bd4f573a2ba1dbb9e01b9c1b809be9
Valdir Stumm Junior <stummjr@gmail.com>
2018-10-09 23:10:52 -0300

    prepare package to be published on pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 import setuptools
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 
 setuptools.setup(
     name='flake8-scrapy',
     license='MIT',
     version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Valdir Stumm Junior',
     author_email='stummjr@gmail.com',
     url='http://github.com/stummjr/flake8-scrapy',
-    py_modules=['flake8_scrapy'],
+    py_modules=[
+        'flake8_scrapy',
+        'finders',
+        'finders.domains',
+        'finders.oldstyle',
+    ],
     entry_points={
         'flake8.extension': [
             'SCP0 = flake8_scrapy:ScrapyStyleChecker',
@@ -20,8 +30,6 @@ setuptools.setup(
         'Framework :: Flake8',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
